06a1696f7947b0da23220242b46dab2e0b1e88ab
Xintao <wxt1994@126.com>
2021-09-03 23:08:35 +0800

    fix bug: wrong root path in train.py

--- basicsrexamples/train.py
+++ basicsrexamples/train.py
@@ -7,5 +7,5 @@ import basicsrexamples.models
 from basicsr.train import train_pipeline
 
 if __name__ == '__main__':
-    root_path = osp.abspath(osp.join(__file__, osp.pardir))
+    root_path = osp.abspath(osp.join(__file__, osp.pardir, osp.pardir))
     train_pipeline(root_path)
